4ada98d6a08a6f37a7dc54fc52a17cad42ac735f
ivasslides <ivasslides@chonkster>
2024-11-14 10:37:33 -0500

    11/14 classwork

--- python/credentials.py
+++ python/credentials.py
@@ -2,7 +2,6 @@ import boto3
 
 # creating a boto3 client for the sqs service -> communitcation service -> messages are wrapped in json 
 sqs = boto3.client('sqs', region_name = 'us-east-2',
-    aws_access_key_id='AKIAWNJE4XNUMNELGYVO',
-    aws_secret_access_key='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'
+    
     )
 
