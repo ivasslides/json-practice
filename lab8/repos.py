8cc71705a63f5bcb72773be8291f25525e729b29
ivasslides <ivasslides@chonkster>
2024-11-23 18:16:53 -0500

    lab8 work

--- lab8/repos.py
+++ lab8/repos.py
@@ -2,19 +2,14 @@
 
 import json
 
-fields = ["name", "html_url", "updated_at", "visibility"]
-
-# read file in as json 
+# read file in as json, limiting to first 5 lines
 with open("../data/schacon.repos.json", "r") as file: 
      data = json.load(file)[:5] 
 
 
-# creating csv file and appending 4 fields to each line 
+# creating csv file and appending the 4 fields to each line 
 with open("chacon.csv", "w") as csv_file: 
-     # limiting to 5 lines
      for item in data:
-         #line = [str(item.get(field, "")) for field in fields]
-         #csv_file.write(",".join(line) + "\n")
          line = f"{item.get('name', '')},{item.get('html_url', '')},{item.get('updated_at', '')},{item.get('visibility', '')}\n"
          csv_file.write(line)
 
